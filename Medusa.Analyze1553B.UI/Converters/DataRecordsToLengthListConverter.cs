e8ab347a6df28913f84404c8d14a2e3e209ab0a8
UjeenSorogovets <Ganozri@users.noreply.github.com>
2020-09-24 16:26:43 +0300

    m: added graphics

---
+++ Medusa.Analyze1553B.UI/Converters/DataRecordsToLengthListConverter.cs
@@ -0,0 +1,48 @@
+﻿using DynamicData;
+using LiveCharts.Geared;
+using Medusa.Analyze1553B.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Medusa.Analyze1553B.UI.Converters
+{
+    [ValueConversion(typeof(DataRecord[]),typeof(double[]))]
+    public class DataRecordsToLengthListConverter : IValueConverter
+    {
+
+        public double[] ArrayOfLength;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var x = (DataRecord[])value;
+            if (value!=null && x.Length > 0)
+            {
+            ArrayOfLength = new double[x.Length];
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                ArrayOfLength[i] = x[i].Cw1.Length;
+                //ArrayOfLength[i] = x[i].MonitorTime;
+            }
+            var Values = ArrayOfLength.AsGearedValues();
+
+            Values.WithQuality(Quality.Low);
+            
+            return Values;    
+            }
+            else
+            {
+                return null;
+            }
+           
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
