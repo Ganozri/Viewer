c664008438ee42b3c1497d02d0f117d3a666a176
UjeenSorogovets <Ganozri@users.noreply.github.com>
2020-09-24 14:56:50 +0300

    m: ui update

---
+++ LiveChartsTest/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Geared;
+using LiveCharts.Helpers;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LiveChartsTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            int x = 10000;
+            double[] d = new double[x];
+            for (int i = 0; i < x; i++)
+            {
+                d[i] = i;
+            }
+
+            Values = d.AsGearedValues();
+
+            Values.WithQuality(Quality.Low);
+            DataContext = this;
+        }
+
+    public GearedValues<double> Values { get; set; }
+}
+}
+
+
---
+++ Medusa.Analyze1553B.UI/Controls/DataRecordsList.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Medusa.Analyze1553B.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for DataRecordsList.xaml
+    /// </summary>
+    public partial class DataRecordsList : CommonPageUserControl
+    {
+        public DataRecordsList()
+        {
+            InitializeComponent();
+        }
+    }
+}
