0f09e5ff89e9ea62bb091db59f8664bf0f37c390
UjeenSorogovets <Ganozri@users.noreply.github.com>
2020-10-01 16:34:43 +0300

    s: ui update

--- Medusa.Analyze1553B.UI/Controls/DataRecordsList.xaml.cs
+++ Medusa.Analyze1553B.UI/Controls/DataRecordsList.xaml.cs
@@ -1,4 +1,14 @@
-﻿namespace Medusa.Analyze1553B.UI.Controls
+﻿using Medusa.Analyze1553B.Common;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Forms.VisualStyles;
+using System.Windows.Media;
+namespace Medusa.Analyze1553B.UI.Controls
 {
     /// <summary>
     /// Interaction logic for DataRecordsList.xaml
@@ -8,6 +18,79 @@
         public DataRecordsList()
         {
             InitializeComponent();
+
+            myListView.ItemContainerGenerator.StatusChanged += new EventHandler(ContainerStatusChanged);
+        }
+        
+        private void ContainerStatusChanged(object sender, EventArgs e)  
+        {  
+            if (myListView.ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated)  
+            {
+
+                //MessageBox.Show(x.ToString());
+                //if (myListView.Items.Count>101)
+                //{
+                //      ChangeColorByIndex(40, Colors.AliceBlue);
+                //}
+
+
+              
+
+                
+                    
+             
+                   
+                
+            }  
+        }
+
+        private List<object> GetVisibleItemsFromListbox(ListView listBox)
+        {
+            var items = new List<object>();
+        
+            foreach (var item in listBox.Items)
+            {
+                if (IsUserVisible((ListViewItem)listBox.ItemContainerGenerator.ContainerFromItem(item), listBox))
+                {
+                    items.Add(item);
+                }
+                else if (items.Any())
+                {
+                    break;
+                }
+            }
+        
+            return items;
+        }
+
+        private static bool IsUserVisible(FrameworkElement element, FrameworkElement container)
+        {
+            if (!element.IsVisible)
+                return false;
+        
+            Rect bounds =
+                element.TransformToAncestor(container).TransformBounds(new Rect(0.0, 0.0, element.ActualWidth, element.ActualHeight));
+            var rect = new Rect(0.0, 0.0, container.ActualWidth, container.ActualHeight);
+            return rect.Contains(bounds.TopLeft) || rect.Contains(bounds.BottomRight);
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+           //var index = 100;
+           //var x = (DataRecord)myListView.Items[25];
+           //ListViewItem row = myListView.ItemContainerGenerator.ContainerFromIndex(index) as ListViewItem;
+          
+     
+           
+             var x = GetVisibleItemsFromListbox(myListView);
+           MessageBox.Show(x.Count.ToString());
+            
+        }
+
+        private void ChangeColorByIndex(int index, System.Windows.Media.Color color)
+        {
+            ListViewItem row = myListView.ItemContainerGenerator.ContainerFromIndex(index) as ListViewItem;
+            row.Background = new SolidColorBrush(color);
         }
     }
 }
---
+++ Medusa.Analyze1553B.UI/Converters/CommandWordToColorConverter.cs
@@ -0,0 +1,39 @@
+﻿using DynamicData;
+using LiveCharts.Geared;
+using Medusa.Analyze1553B.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace Medusa.Analyze1553B.UI.Converters
+{
+    public class CommandWordToColorConverter : IValueConverter
+    {
+         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var x = (DataRecord[])value;
+
+            return "Red";
+            //if (x.Cw1.Length==32)
+            //{
+            //    return "Blue";
+            //}
+            //else
+            //{
+            //     return "Red";
+            //}
+           
+           
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
