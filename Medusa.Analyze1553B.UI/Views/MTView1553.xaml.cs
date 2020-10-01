4f55d9be1653058bb77d5bee823bd3ee40f79bf4
UjeenSorogovets <Ganozri@users.noreply.github.com>
2020-10-01 08:22:57 +0300

    s: refactoring

---
+++ Medusa.Analyze1553B.UI/Views/MTView1553.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Medusa.Analyze1553B.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for MTView1553.xaml
+    /// </summary>
+    public partial class MTView1553 : CommonPageUserControl
+    {
+        public MTView1553()
+        {
+            InitializeComponent();
+        }
+    }
+}
